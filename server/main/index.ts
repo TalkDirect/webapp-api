c2665c5132128ccd11cd3bf81b89324bb42d966a
Jacob Coby <42078078+cobyvevo@users.noreply.github.com>
2023-12-22 16:33:29 +0000

    Basic functionality for creating and joining sessions

--- server/main/index.ts
+++ server/main/index.ts
@@ -1,20 +1,82 @@
 import express, { Express, Request, Response } from "express";
+import { Session } from "./modules/Session"
+
+
 const app: Express = express();
+var allSessions = new Map<string, Session>();
+
+function CreateNewSession(sessionid: string) {
+
+	let newsession: Session = new Session();
+	allSessions.set(sessionid, newsession);
+	return newsession
+
+}
+
+function JoinSession(sessionid: string, ip: string) {
+
+	var desiredsession = allSessions.get(sessionid)
+	if (desiredsession !== undefined) {
+		desiredsession.AddClient(ip)
+		return true //return if the session was joined successfully or not
+	}
+	return false
+
+}
 
 app.get("/api/host/sid=:sessionid", (req: Request, res: Response) => {
-	res.status(201).json(
-		{
-			sessionid: req.params.sessionid, 
+
+	if (req.params.sessionid !== undefined && req.ip !== undefined) {
+
+		let sessionid: string = req.params.sessionid;
+		let ip: string = req.ip;
+
+		//create new session
+		let newsession = CreateNewSession(sessionid) 
+
+		//attempt to join newly created session
+		let success = JoinSession(sessionid, req.ip)
+
+		if (success) {
+			res.status(201).json(
+				{
+					status: "Successfully created session",
+					sessionid: req.params.sessionid, 
+				}
+			);
+			return; //guard clause
 		}
-	)
+
+	}
+
+	res.status(400).send()
+
 });
 
 app.get("/api/join/sid=:sessionid", (req: Request, res: Response) => {
-	res.status(202).json(
-		{
-			sessionid: req.params.sessionid,
-		}
-	)
+	
+	if (req.params.sessionid !== undefined && req.ip !== undefined) {
+		
+		let sessionid: string = req.params.sessionid;
+		let ip: string = req.ip;
+
+ 		//attempt to join 
+		let success = JoinSession(sessionid, ip);
+
+		if (success) {
+			res.status(202).json(
+				{
+					status: "Successfully joined session",
+					sessionid: req.params.sessionid,
+				}
+			);
+			return; //guard clause 
+		} 
+
+	}
+
+	res.status(400).send()
+
 });
 
 app.listen(9999, () => {
---
+++ server/main/modules/Session.ts
@@ -0,0 +1,30 @@
+interface Client {
+	address: string;
+	name: string;
+}
+
+export class Session {
+
+	sessionid: string = "123abc";
+	clients: Array<Client> = [];
+
+	AddClient(address: string) {
+		//Create a new client and add to array of currently connected clients
+		var newClient: Client = {address: address, name: "User"};
+		this.clients.push(newClient);
+		console.log("Added client " + address);
+	}
+
+	RemoveClient(address: string) {
+		//find client within array, and remove them
+		for (let i = 0; i < this.clients.length; i++) {
+			if (this.clients[i].address = address) {
+				this.clients.splice(i-1, i);
+				console.log("Removed client " + address);
+				break;
+			}
+		}
+		console.log("Remove client " + address + " failed.");
+	}
+
+}
