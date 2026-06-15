77f649928e1a39f4c12b4484286c74d1bf02e06e
Chieffz <bitmod3@gmail.com>
2026-06-15 00:12:47 -0400

    Trying out change to start work on full file sharing

--- web/src/WebSocketService.tsx
+++ web/src/WebSocketService.tsx
@@ -60,6 +60,7 @@ export class useSocket {
         // First byte of data will ALWAYS be Data Identifier
         if (this.socket == null) return;
         const DataID = data.at(0);
+        data[0] = DataIdentifier.FILE;
 
         // If we're planning on sending over a string put it into a byte array; else throw it into a int array (32 bit array)
         if (DataID == DataIdentifier.STRING) {
@@ -68,6 +69,8 @@ export class useSocket {
             return;
         }
         else if (DataID == DataIdentifier.FILE) { // For now do not handle Files yet for sending to C++ desktop app
+            this.socket.send(new Uint8Array(data));
+            console.log("Sent File over")
             return;
         }
         const bufferArray = new Uint32Array(data);
